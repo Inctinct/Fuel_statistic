46d94b414383cd8a4d36b3c7c207c5cccc09e7e2
Inctinct <dmitriy.makeychik@mail.ru>
2023-03-01 13:24:16 +0300

    add models of firm, fuel, refueling for db

--- fuel/statistic/models.py
+++ fuel/statistic/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Firm(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Fuel(models.Model):
+    name = models.CharField(max_length=15)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Refueling(models.Model):
+    adress = models.TextField()
+    firm = models.ForeignKey(Firm, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.adress
+
